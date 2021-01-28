4d8313b7127aaa9632600128c8c6850669cc3427
Biced <biced.nff@gmail.com>
2021-01-28 18:33:35 +0200

    paths and portrait lock test

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 // import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.118/build/three.module.js';
 import * as THREE from './build/three.module.js';
-import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
+import {FBXLoader} from './jsm/loaders/FBXLoader.js';
+// import {FBXLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/FBXLoader.js';
 // import TWEEN from 'https://cdn.jsdelivr.net/npm/@tweenjs/tween.js@18.5.0/dist/tween.esm.js';
 
 
